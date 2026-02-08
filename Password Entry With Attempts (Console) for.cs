70910eed9e11e850a769bd8727991e270ca394fa
Romchik <dotaplus836@gmail.com>
2026-02-08 14:00:21 +0300

    6
    Password Entry With Attempts

---
+++ Password Entry With Attempts (Console) for.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Zed
+{
+    class Program
+    {
+        private const int NumberAttempts = 3;
+        private const string Password = "6996";
+
+        private static void Main(string[] args)
+        {
+            bool isAuthorized = false;
+
+            Console.WriteLine("Приветствую");
+            Console.WriteLine("Войдите в аккаунт");
+
+            for (int i = 1; i <= NumberAttempts; i++)
+            {
+                Console.Write("\nВведите пароль: ");
+                string userInputPassword = Console.ReadLine();
+
+                if (userInputPassword is null)
+                {
+                    Console.WriteLine("Ошибка ввода");
+                    continue;
+                }
+
+                if (userInputPassword == Password)
+                {
+                    isAuthorized = true;
+                    Console.WriteLine("\nВы успешно вошли в аккаунт");
+                    break;
+                }
+
+                Console.WriteLine($"Неверный пароль. Осталось: {NumberAttempts - i} попыток");
+            }
+
+            if (!isAuthorized)
+            {
+                Console.WriteLine("\nПопытки закончились. Доступ запрещён");
+            }
+        }
+    }
+}
